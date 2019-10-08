dd9a0a8af377d1f116a7b869787c0ebad3333416
cryptoprof <cryptoprof@gmail.com>
2019-10-09 00:06:19 +0300

    current model element url

--- src/resources/views/table/cells/link.blade.php
+++ src/resources/views/table/cells/link.blade.php
@@ -1,4 +1,19 @@
-@php
-    $json=json_decode($value, true);
-@endphp
-<td><a href="{{ $options['url_prefix'].$json[$options['url_postfix']]}}">{{$json[$options['value']]}}</a></td>
+@if($options['type']=='json')
+    @php
+        $json=json_decode($value, true);
+    @endphp
+    <td><a href="{{ $options['url_prefix'].$json[$options['url_postfix']]}}">{{$json[$options['value']]}}</a></td>
+@elseif($options['type']=='current_model_detail')
+    @php
+        $base_route = $table['base_route'] ?? '';
+    @endphp
+    <td>
+        <a
+                title="{{ trans('View this record') }}"
+                href="{{ Route::has($base_route.'.show') ? route($base_route.'.show', $id) : '#' }}"
+                data-id="{{ $value }}"
+                data-action="view-item">
+            {{$value}}
+        </a>
+    </td>
+@endif
