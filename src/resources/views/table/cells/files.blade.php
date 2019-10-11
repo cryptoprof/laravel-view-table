9d99acc4ed56538a376d6e641de52dbfa2362477
cryptoprof <cryptoprof@gmail.com>
2019-10-11 23:54:10 +0300

    fix

--- src/resources/views/table/cells/files.blade.php
+++ src/resources/views/table/cells/files.blade.php
@@ -4,11 +4,12 @@
 <td>
     @if($json)
         @foreach($json as $file)
-
-            <a href="{{ $options['url_prefix'].$file}}">
-                <i class="fa fa-file-text-o u-text-mute u-mr-xsmall pr-2" style="float: left;"></i>
-                {{$file}}
-            </a>
+            <div class="col-12">
+                <a href="{{ $options['url_prefix'].$file}}">
+                    <i class="fa fa-file-text-o u-text-mute u-mr-xsmall pr-2" style="float: left;"></i>
+                    {{$file}}
+                </a>
+            </div>
         @endforeach
     @endif
 </td>
