5d34a1990b9e1b734859b2f0e4d478381697ff37
cryptoprof <cryptoprof@gmail.com>
2019-10-14 20:16:57 +0300

    status field

---
+++ src/resources/views/table/cells/status.blade.php
@@ -0,0 +1,4 @@
+<td class="text-center">
+    <span class="badge" style="background: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['color']}};
+                               color: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['font-color']}}"
+    >{{ ($value)?$value:'Новое поручение' }}</span></td>
