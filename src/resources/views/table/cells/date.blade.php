c13ca92baee59aca1437d234185401bd0ed7f221
cryptoprof <cryptoprof@gmail.com>
2019-10-16 22:00:03 +0300

    Передача в расчет дедлайна айдишника задачи

--- src/resources/views/table/cells/date.blade.php
+++ src/resources/views/table/cells/date.blade.php
@@ -1,8 +1,8 @@
 <td>
     {{\Carbon\Carbon::parse($value)->format($options['format'])}}
-    @if(isset($options['showDiff']))
+    @if($options['showDiff']!='')
         @php
-            $diff=App\Task::getDeadlinCountAttribute($value);
+            $diff=App\Task::getDeadlineCountAttribute($value,$id);
         @endphp
         {!! $diff !!}
     @endif
