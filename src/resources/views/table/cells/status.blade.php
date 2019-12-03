cb596d80babbaa533486ee3c2fee01ae9a4919c1
varlog <cryptoprof@gmail.com>
2019-12-03 22:39:21 +0300

    update

--- src/resources/views/table/cells/status.blade.php
+++ src/resources/views/table/cells/status.blade.php
@@ -1,4 +1,7 @@
 <td class="c-table__cell text-center">
-    <span class="c-badge c-badge--small " style="background: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['color']}};
-                               color: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['font-color']}}"
-    >{{ ($value)?$value:'Новое поручение' }}</span></td>
+    @if($value)
+        <span class="c-badge c-badge--small " style="background: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['color']}};
+                                   color: {{\App\Classess\Helpers\StatusConfig::$STATUS[$value]['font-color']}}"
+        >{{ $value }}</span>
+    @endif
+</td>
